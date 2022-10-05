1df2845a6e813d20b732cbf06c20018588a5e051
bombermon <npototun0@gmail.com>
2022-10-05 17:24:07 +0300

    two arrays

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,15 +1,36 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Library {
     List<Book> allBooks = new ArrayList<Book>();
     ArrayList<ReaderCard> readerCards = new ArrayList<ReaderCard>();
 
+    List<Book> BooksInStock = new ArrayList<Book>();
+    List<Book> BooksInHands = new ArrayList<Book>();
+
+
     public void addBook(Book book){
-        allBooks.add(book);
+        if (Arrays.asList(allBooks).contains(book)){
+            System.out.println("Такая книга уже есть");
+        }
+        else{
+            allBooks.add(book);
+            BooksInStock.add(book);
+        }
+
     }
 
     public void deleteBook(Book book){
-        allBooks.remove(book);
+        if (Arrays.asList(BooksInStock).contains(book)){
+            allBooks.remove(book);
+            BooksInStock.remove(book);
+        }
+        else {
+            System.out.println("Книга на руках у читателя, ее нельзя удалить.");
+        }
+
+
+
     }
 }
